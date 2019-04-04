795606294d49bef0a9f6a5480fc3307d3f49d993
malin@souche.com <malin@souche.com>
2019-04-04 18:14:23 +0800

    init

---
+++ src/main/java/com/teach/manager/teachmanager/TeachInteractiveManagerApplication.java
@@ -0,0 +1,16 @@
+package com.teach.manager.teachmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author malin
+ */
+@SpringBootApplication
+public class TeachInteractiveManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeachInteractiveManagerApplication.class, args);
+    }
+
+}
