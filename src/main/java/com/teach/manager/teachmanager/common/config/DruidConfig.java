2afe6b5cd9bbd618b45c5eadc1f4c85fdba38e11
malin@souche.com <malin@souche.com>
2019-04-09 09:49:05 +0800

    修改bug

---
+++ src/main/java/com/teach/manager/teachmanager/common/config/DruidConfig.java
@@ -0,0 +1,12 @@
+package com.teach.manager.teachmanager.common.config;
+
+/**
+ * @program: teach-interactive-manager
+ * @ClassName: DruidConfig
+ * @description: druid配置类
+ * @author: malin
+ * @create: 2019-04-04 18:49
+ **/
+
+public class DruidConfig {
+}
---
+++ src/main/java/com/teach/manager/teachmanager/controller/WelcomeTeach.java
@@ -0,0 +1,12 @@
+package com.teach.manager.teachmanager.controller;
+
+/**
+ * @program: teach-interactive-manager
+ * @ClassName: WelcomeTeach
+ * @description: 欢迎界面测试使用
+ * @author: malin
+ * @create: 2019-04-04 18:36
+ **/
+
+public class WelcomeTeach {
+}
