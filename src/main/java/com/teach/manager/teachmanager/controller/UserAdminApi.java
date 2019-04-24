638cec5ca7e0179e04a1e83bf0aacdbe15122d9a
navigator <15313165225@163.com>
2019-04-24 14:17:01 +0800

    添加用户权限相关表格

---
+++ src/main/java/com/teach/manager/teachmanager/controller/UserAdminApi.java
@@ -0,0 +1,23 @@
+package com.teach.manager.teachmanager.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+/**
+ * @program: teach-interactive-manager
+ * @ClassName: UserAdminApi
+ * @description: 用户登录注册等接口
+ * @author: malin
+ * @create: 2019-04-24 10:29
+ **/
+@Controller
+@Slf4j
+public class UserAdminApi {
+
+    @PostMapping
+    public void login() {
+
+    }
+
+}
